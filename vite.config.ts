b94f92333302b6281111249cfa53bcf78ce5cf29
Sue-svbd <susannacapacchione@gmail.com>
2026-03-24 15:11:09 +0100

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-  base: "/Portfolio-2026/",
+  base: "/",
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
